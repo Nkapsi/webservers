4f80dacd55cbcde35f1d38148b32b8e9614d2213
Nkapsi <nkapsi13@gmail.com>
2023-10-04 14:06:39 -0400

    Update phpfile.php

--- website/phpfile.php
+++ website/phpfile.php
@@ -4,14 +4,12 @@
         <title>Form response</title>
     </head>
  <body>
-    <p>GET: <?= var_dump($_GET) ?></p>
-    <p>POST: <?= var_dump($_POST) ?></p>
-
-    <p>Your card type is: <?=$_POST["cctype"]?></p>
-    <p>Your credit card number is: <?=$_POST["ccn"]?></p>
-    <p>Your name is: <?=$_POST["name"]?></p>
-    <p>Your cvv code is: <?=$_POST["cvv"] ?></p>
-    <p>Your country is: <?=$_POST["Countries"] ?></p>
+   
+    <p>Your card type is: <?= htmlspecialchars($_POST["cctype"]); ?></p>
+    <p>Your credit card number is: <?= tmlsepcialchars($_POST["ccn"]); ?></p>
+    <p>Your name is: <?= htmlsepcialchars($_POST["name"]); ?></p>
+    <p>Your cvv code is: <?= htmlsepcialchars($_POST["cvv"]); ?></p>
+    <p>Your country is: <?= htmlsepcialchars($_POST["Countries"]); ?></p>
 
  </body>
 </html>
