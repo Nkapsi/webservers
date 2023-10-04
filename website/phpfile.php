38daf9a973ef33d89d8b61d46c79b3cb47bad7de
Nkapsi <nkapsi13@gmail.com>
2023-10-04 14:25:23 -0400

    Update phpfile.php

--- website/phpfile.php
+++ website/phpfile.php
@@ -6,10 +6,10 @@
  <body>
    
     <p>Your card type is: <?= htmlspecialchars($_POST["cctype"]); ?></p>
-    <p>Your credit card number is: <?= tmlsepcialchars($_POST["ccn"]); ?></p>
-    <p>Your name is: <?= htmlsepcialchars($_POST["name"]); ?></p>
-    <p>Your cvv code is: <?= htmlsepcialchars($_POST["cvv"]); ?></p>
-    <p>Your country is: <?= htmlsepcialchars($_POST["Countries"]); ?></p>
+    <p>Your credit card number is: <?= htmlspecialchars($_POST["ccn"]); ?></p>
+    <p>Your name is: <?= htmlspecialchars($_POST["name"]); ?></p>
+    <p>Your cvv code is: <?= htmlspecialchars($_POST["cvv"]); ?></p>
+    <p>Your country is: <?= htmlspecialchars($_POST["Countries"]); ?></p>
 
  </body>
 </html>
