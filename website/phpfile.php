aaacdb819a657bc4a83cdabf4973c5b6bce13c17
Nkapsi <nkapsi13@gmail.com>
2023-10-04 13:54:40 -0400

    changed php response file

--- website/phpfile.php
+++ website/phpfile.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Form response</title>
+    </head>
  <body>
     <p>GET: <?= var_dump($_GET) ?></p>
     <p>POST: <?= var_dump($_POST) ?></p>
+
+    <p>Your card type is: <?=$_POST["cctype"]?></p>
+    <p>Your name is: <?=$_POST["name"]?></p>
+    <p>Your cvv code is: <?=$_POST["cvv"] ?></p>
+    <p>Your country is: <?=$_POST["Countries"] ?></p>
+
  </body>
 </html>
