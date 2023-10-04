de2ba48324f46e3bdaac2f7f2d12e94c3b5bdacf
Nkapsi <nkapsi13@gmail.com>
2023-10-04 13:17:38 -0400

    Update phpfile.php

--- website/phpfile.php
+++ website/phpfile.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
  <body>
-    <p><?= var_dump($_GET) ?></p>
-    <p><?= var_dump($_POST) ?></p>
+    <p>GET: <?= var_dump($_GET) ?></p>
+    <p>POST: <?= var_dump($_POST) ?></p>
  </body>
 </html>
