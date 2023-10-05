d6fe5fb34042dc6db09eea62e6894b3e03fc5e93
Nick <nkapsi13@gmail.com>
2023-10-05 09:17:47 -0400

    test file added

--- website/index.php
+++ website/index.php
@@ -1,83 +1,89 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <script>
-            function modify() {
-                document.getElementById("para").innerHTML = "New text appeared, wow!";
-            }
-            function visibility() {
-               var x = document.getElementById("hide")
-                
-                if (x.style.display === "none") {
-                     x.style.display = "block";
-                } else {
-                      x.style.display = "none";
-                }
-            }
-            function img1()  {
-                document.getElementById("loading1").src = "loading.gif"
-            }
-            function img2()  {
-                document.getElementById("loading1").src = "blank.jpg"
-            }
-            function txtchange(elem)   {
-                elem.innerHTML="It works!";
-            }
-            function loadtxt()  {
-                const xhttp = new XMLHttpRequest();
-                xhttp.onload = function() {
-                    document.getElementById("changebyajax").innerHTML = 
-                    this.responseText;
-                }
-                xhttp.open("GET", "ajaxtext.txt");
-                xhttp.send();
-            }
-        </script>
-        <center>
-            <h1><FONT COLOR=BLACK>HOMEPAGE</FONT></h1>
-            <br/>
-            <h2><FONT COLOR=#0e77c9>By: Nick K</FONT></h2>
-            <br/><br/>
-    </head>
+	<head>
+		<script>
+			function modify() {
+			    document.getElementById("para").innerHTML = "New text appeared, wow!";
+			}
+			function visibility() {
+			   var x = document.getElementById("hide")
 
-    <body style="background-image: url('backg.jpg');">
-            <p><FONT COLOR=#f6ff00>Welcome to the home page, more to come...</FONT></p>
-            <img id="loading1" src="loading.gif" alt="Loading" width="45" height="45">
-            <br/><br/>
-            <p id="para">Keep an eye on this text</p>
-            <br/>
-            <p id="hide">This text may dissapear..</p>
-        </center>
-        <div>
-           <p>Here's what this page includes: </p>
-           <ul>
-               <li>Two level headings</li>
-               <li>A div</li>
-               <li>A paragraph</li>
-               <li>Images</li>
-               <li>List</li> 
-               <li>Link to other site</li>
-           </ul>
-       </div>
-        <center>
-            <a href="winner.html" target="_blank"> Click this link! </a>
-            <br/><br/>
-            <button onclick="alert('ALERT')">Press for alert</button>
-            <button onclick="modify()">Click here to change something</button>
-            <button onclick="visibility()">Click here to hide/show some text</button>
-            <button onmouseenter="img2()" onmouseleave="img1()">Hover over me to break something</button>
-            <br/>
-            <button onclick="txtchange(this)">click here to change this text</button>
-            <br/><br/>
-            
-            <div id="changebyajax">
-                <p>This text will change if AJAX is working as it should</p>
-                <button type="button" onclick="loadtxt()">Click to request a txt file</button>
-            </div>
-        </center>
-            <div>
-                <h2>Lab 5 PHP section:</h2>
-                <p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
-            </div>
-    </body> 
+			    if (x.style.display === "none") {
+			         x.style.display = "block";
+			    } else {
+			          x.style.display = "none";
+			    }
+			}
+			function img1()  {
+			    document.getElementById("loading1").src = "loading.gif"
+			}
+			function img2()  {
+			    document.getElementById("loading1").src = "blank.jpg"
+			}
+			function txtchange(elem)   {
+			    elem.innerHTML="It works!";
+			}
+			function loadtxt()  {
+			    const xhttp = new XMLHttpRequest();
+			    xhttp.onload = function() {
+			        document.getElementById("changebyajax").innerHTML =
+			        this.responseText;
+			    }
+			    xhttp.open("GET", "ajaxtext.txt");
+			    xhttp.send();
+			}
+		</script>
+		<center>
+			<h1><font color="BLACK">HOMEPAGE</font></h1>
+			<br />
+			<h2><font color="#0e77c9">By: Nick K</font></h2>
+			<br />
+			<br />
+		</center>
+	</head>
+
+	<body style="background-image: url('backg.jpg');">
+		<center>
+			<p><font color="#f6ff00">Welcome to the home page, more to come...</font></p>
+			<img id="loading1" src="loading.gif" alt="Loading" width="45" height="45" />
+			<br />
+			<br />
+			<p id="para">Keep an eye on this text</p>
+			<br />
+			<p id="hide">This text may dissapear..</p>
+		</center>
+		<div>
+			<p>Here's what this page includes:</p>
+			<ul>
+				<li>Two level headings</li>
+				<li>A div</li>
+				<li>A paragraph</li>
+				<li>Images</li>
+				<li>List</li>
+				<li>Link to other site</li>
+			</ul>
+		</div>
+		<center>
+			<a href="winner.html" target="_blank"> Click this link to win a prize! </a>
+			<br />
+			<br />
+			<button onclick="alert('ALERT')">Press for alert</button>
+			<button onclick="modify()">Click here to change something</button>
+			<button onclick="visibility()">Click here to hide/show some text</button>
+			<button onmouseenter="img2()" onmouseleave="img1()">Hover over me to break something</button>
+			<br />
+			<button onclick="txtchange(this)">click here to change this text</button>
+			<br />
+			<br />
+
+			<div id="changebyajax">
+				<p>This text will change if AJAX is working as it should</p>
+				<button type="button" onclick="loadtxt()">Click to request a txt file</button>
+			</div>
+		</center>
+		<div>
+			<h2>Lab 5 PHP section:</h2>
+			<p><?= $_SERVER['HTTP_USER_AGENT'] ?></p>
+		</div>
+	</body>
 </html>
--- website/phpfile.php
+++ website/phpfile.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Form response</title>
-    </head>
- <body>
-   
-    <p>Your card type is: <?= htmlspecialchars($_POST["cctype"]); ?></p>
-    <p>Your credit card number is: <?= htmlspecialchars($_POST["ccn"]); ?></p>
-    <p>Your name is: <?= htmlspecialchars($_POST["name"]); ?></p>
-    <p>Your cvv code is: <?= htmlspecialchars($_POST["cvv"]); ?></p>
-    <p>Your country is: <?= htmlspecialchars($_POST["Countries"]); ?></p>
-
- </body>
+   <head>
+      <title>Form response</title>
+   </head>
+   <body>
+      <p>Your card type is: <?= htmlspecialchars($_POST["cctype"]); ?></p>
+      <p>Your credit card number is: <?= htmlspecialchars($_POST["ccn"]); ?></p>
+      <p>Your name is: <?= htmlspecialchars($_POST["name"]); ?></p>
+      <p>Your cvv code is: <?= htmlspecialchars($_POST["cvv"]); ?></p>
+      <p>Your country is: <?= htmlspecialchars($_POST["Countries"]); ?></p>
+   </body>
 </html>
